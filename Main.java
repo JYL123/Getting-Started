173e48bc8c099b7eb3de1804f9aefc0abf4accc6
Li JiaYao <li474412210@gmail.com>
2018-02-25 14:22:04 +0800

    Eliminate space

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
 	
     /**
-     *  Scan input and initialize the matrix
+     *Scan input and initialize the matrix
      */
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
